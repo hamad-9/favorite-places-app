570b239334aa7735ff42a4e1d4704b506d33518f
Hamad Numan <78150971+hamad-9@users.noreply.github.com>
2022-05-23 07:23:30 +0300

    general structure

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,12 +1,13 @@
 import { FlatList, View, Text, StyleSheet } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 function PlacesList({ places }) {
   if (!places || places.length == 0) {
     return (
       <View style={styles.fallbackContainer}>
         <Text style={styles.fallbackText}>
-          no places added yet, start adding some
+          No places added yet, start adding some!
         </Text>
       </View>
     );
@@ -31,5 +32,6 @@ const styles = StyleSheet.create({
 
   fallbackText: {
     fontSize: 16,
+    color: Colors.primary200,
   },
 });
